0e2eb3ed8cab9e82afaf3fbda9d73d1ca19dba63
Henrique Martins Botelho <henriquemartinsbotelho@gmail.com>
2022-08-19 13:13:17 -0700

    first

---
+++ app/src/main/java/com/bromodomo/second_java/MainActivity.java
@@ -0,0 +1,24 @@
+package com.bromodomo.second_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void alterarTexto(View view){
+        TextView texto = findViewById(R.id.textView);
+        texto.setText("Olá, Renato Bueno!");
+    }
+
+
+}
